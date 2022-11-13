e644685382f93a08e12f740cb1bb163f62f35005
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-13 10:03:52 +0600

    inital setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0fcfce",
+        secondary: "#19d3ae",
+        neutral: "#3a4256",
+      },
+    },
+  },
+  plugins: [],
+};
